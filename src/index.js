b0936a38bda2eaafa9ac84d716233731b9e0d483
rozie321 <107440478+rozie321@users.noreply.github.com>
2024-06-18 10:45:50 +0100

    timezones with respective cities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-function
+function SelectCity(event){
+    if (event.target.value.length>0){
+        if(event.target.value==="paris"){
+            let parisTime = moment()
+                .tz("Europe/paris")
+                .format("dddd, MMMM D, YYYY hh:mm A");
+            alert(`It is ${parisTime} in Paris/France`);
+        }
+        if(event.target.value==="sydney"){
+            let sydneyTime = moment()
+                .tz("Australia/Sydney")
+                .format("dddd, MMMM D, YYYY hh:mm A");
+            alert(`It is ${sydneyTime} in Sydney/Australia`);
+        }
+        if(event.target.value==="tokyo"){
+            let tokyoTime = moment()
+                .tz("Asia/Tokyo")
+                .format("dddd, MMMM D, YYYY hh:mm A");
+            alert(`It is ${tokyoTime} in Tokyo/Japan`);
+        }
+    }
+
+}
+let citiesSelect= document.querySelector("#cities")
+citiesSelect.addEventListener("change",SelectCity);
+
